bc663ffcb9f678561057fd354e31e39a518306fc
Excelsior2026 <billparris@gmail.com>
2026-03-05 15:23:24 -0800

    Add package version metadata

--- ese/__init__.py
+++ ese/__init__.py
@@ -1,1 +1,7 @@
 """Ensemble Software Engineering (ESE) package."""
+
+from importlib.metadata import version
+
+
+__version__ = version("ese-cli")
+__copyright__ = "Copyright (c) 2026 BagelTech.net"
